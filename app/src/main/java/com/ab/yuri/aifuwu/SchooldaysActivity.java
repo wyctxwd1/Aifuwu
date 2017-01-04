a4061e9837f1b8baf9a4ae2213d14af9e12bb3ee
wyctxwd1 <469104809@qq.com>
2017-01-04 18:02:48 +0800

    第四次提交
    实现点击校历利用dialog弹窗+photoView实现查看大图功能

--- app/src/main/java/com/ab/yuri/aifuwu/SchooldaysActivity.java
+++ app/src/main/java/com/ab/yuri/aifuwu/SchooldaysActivity.java
@@ -7,12 +7,16 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+
+import uk.co.senab.photoview.PhotoView;
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class SchooldaysActivity extends AppCompatActivity {
 
@@ -39,6 +43,31 @@ public class SchooldaysActivity extends AppCompatActivity {
         collapsingToolbar.setTitle("校历");
         Glide.with(this).load(R.drawable.schooldays_title_view).into(schooldaysTitleView);
         Glide.with(this).load(R.drawable.schooldays_content_view).into(schooldaysContentView);
+        schooldaysContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater=LayoutInflater.from(SchooldaysActivity.this);
+                View dialogView=inflater.inflate(R.layout.schooldays_dialog,null);
+                final AlertDialog dialog=new AlertDialog.Builder(SchooldaysActivity.this).create();
+                PhotoView dialogImg= (PhotoView) dialogView.findViewById(R.id.schooldays_dialog_imageView);
+                dialogImg.setImageResource(R.drawable.schooldays_content_view);
+                dialog.setView(dialogView);
+                dialog.show();
+                dialogImg.setOnPhotoTapListener(new PhotoViewAttacher.OnPhotoTapListener() {
+                    @Override
+                    public void onPhotoTap(View view, float x, float y) {
+                        dialog.cancel();
+                    }
+
+                    @Override
+                    public void onOutsidePhotoTap() {
+                        dialog.cancel();
+                    }
+                });
+
+
+            }
+        });
 
 
 
@@ -66,4 +95,7 @@ public class SchooldaysActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
